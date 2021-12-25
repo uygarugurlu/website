27146e57440313f2c953e017830fc92396d66b40
Uygar Uğurlu <91941934+uygarugurlu@users.noreply.github.com>
2021-12-25 14:39:55 +0300

    Update googleAnalytics.ts

--- config/modules/googleAnalytics.ts
+++ config/modules/googleAnalytics.ts
@@ -1,5 +1,5 @@
 import { InstallOptions } from "vue-analytics"
 
-const GoogleAnalytics: InstallOptions = { id: "G-0SKG74431V" }
+const GoogleAnalytics: InstallOptions = { id: "G-SCW6G0T7B1" }
 
 export default GoogleAnalytics

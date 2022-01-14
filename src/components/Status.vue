855dd62d36a8986787c23f880a7af81d5a936912
Uygar Uğurlu <91941934+uygarugurlu@users.noreply.github.com>
2022-01-14 22:02:36 +0300

    Status.vue güncellendi
    Discord User ID'mi güncelledim.

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -97,7 +97,7 @@ export default Vue.extend({
   async mounted() {
     // Connect to Lanyard Socket API, send heartbeat every 30 seconds and replace the Vue data value with the message using @eggsydev/vue-lanyard module
     this.socket = (await this.$lanyard({
-      userId: "929502560648298496",
+      userId: "930837865733185597",
       socket: true,
     })) as WebSocket
 

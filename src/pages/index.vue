f974e23c759cb8a431162076aa01e9cddd55bfc1
Uygar Uğurlu <91941934+uygarugurlu@users.noreply.github.com>
2022-06-23 22:08:25 +0300

    index.vue Güncellendi!
    Başlıktaki renk ayarı güncellendi.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -76,7 +76,7 @@ export default Vue.extend({
             class="font-semibold text-xl text-gray-900 md:text-3xl dark:text-neutral-200"
           >
             <h1>Self taught</h1>
-            <h1><span class="text-indigo-500">Web Application</span> developer</h1>
+            <h1><span class="text-pink-400">Web Application</span> developer</h1>
           </div>
 
           <p class="text-neutral-500">

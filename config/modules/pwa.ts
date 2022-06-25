2f15d20eadc85400a24e9d66499699cfd2a7da2c
Uygar Uğurlu <91941934+uygarugurlu@users.noreply.github.com>
2022-06-26 00:20:51 +0300

    Update pwa.ts

--- config/modules/pwa.ts
+++ config/modules/pwa.ts
@@ -1,7 +1,7 @@
 export default {
   manifest: {
     name: "uygarugurlu.com.tr",
-    short_name: "uygarugurlu.com.tr",
+    short_name: "Uygar Uğurlu",
     theme_color: "#f56565",
     lang: "en",
   },

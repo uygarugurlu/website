96fad113e787bf671200a53a7544001534bffb50
Uygar Uğurlu <91941934+uygarugurlu@users.noreply.github.com>
2022-06-25 21:02:19 +0300

    Update Default.vue

--- src/components/Navbar/Default.vue
+++ src/components/Navbar/Default.vue
@@ -83,7 +83,7 @@ export default Vue.extend({
       <div class="flex items-center justify-between">
         <SmartLink :href="getTargetLink" class="flex-shrink-0 h-10 w-10">
           <SmartImage
-            src="/assets/icons/icon.png"
+            src="/assets/icons/icon.svg"
             class="rounded-md bg-gray-100 ring-black transition-transform ring-2 ring-opacity-5 sm:transform hover:-rotate-6"
             width="100"
             height="100"
